74f42727942f1ee54c39bac5f3773a94aa4a0835
Jujar Singh <jujarsingh1511@gmail.com>
2026-03-18 11:23:31 +0530

    change in main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def print_hi(name):
 
 if __name__ == "__main__":
     #adding new features to feature-1
+    #some new changes also done for merge conflict
     # some new changes in main
     name = input("Enter Your Name:")
     print_hi(name)
